d91e13632f182e834016fedb68ddffd641eaaf5c
Colleen Murray <colleenkhm@gmail.com>
2022-12-07 12:36:17 -0600

    add music and sound effects

--- rebound.js
+++ rebound.js
@@ -7,6 +7,9 @@ var controls;
 var newButton;
 var difficultySelect;
 var done;
+var snd;
+var music;
+
 var aHeight;
 var pWidth;
 var pHeight;
@@ -19,6 +22,15 @@ var paddleLeft = 228;
 var ballLeft = 100;
 var ballTop = 8;
 var drag = false;
+
+var sndEnabled = false;
+var musicEnabled = false;
+
+var beepX;
+var beepY;
+var beepPaddle;
+var beepGameOver;
+var bgMusic;
 
 window.addEventListener("load", init);
 window.addEventListener("resize", init);
@@ -34,6 +46,8 @@ function init() {
   newButton = document.getElementById("new");
   difficultySelect = document.getElementById("difficulty");
   done = document.getElementById("done");
+  snd = document.getElementById("snd");
+  music = document.getElementById("music");
   layoutPage();
   document.addEventListener("keydown", keyListener, false);
 
@@ -52,8 +66,10 @@ function init() {
     function () {
       setDifficulty(difficultySelect.selectedIndex);
     },
-    false
-  );
+    false);
+
+    snd.addEventListener('click', toggleSound, false);
+    music.addEventListener('click', toggleMusic, false);
 
   timer = requestAnimationFrame(start);
 }
@@ -126,6 +142,7 @@ function detectCollisions() {
 function collisionX() {
   // if ball gets near edge then collision occured
   if (ballLeft < 4 || ballLeft > pWidth - 20) {
+    playSound(beepX);
     return true;
   }
   return false;
@@ -133,6 +150,7 @@ function collisionX() {
 function collisionY() {
   // if ball gets near edge then collision occured
   if (ballTop < 4 || ballLeft > pWidth - 20) {
+    playSound(beepY);
     return true;
   }
   if (ballTop > pHeight - 64) {
@@ -145,12 +163,14 @@ function collisionY() {
       } else {
         dx = 2;
       }
+      playSound(beepPaddle);
       return true;
     } else if (ballLeft >= paddleLeft && ballLeft < paddleLeft + 16) {
       if (dx < 0) dx = -8;
       else {
         dx = 8;
       }
+      playSound(beepPaddle);
       return true;
     } else if (ballLeft >= paddleLeft + 48 && ballLeft <= paddleLeft + 64) {
       if (dx < 0) {
@@ -158,6 +178,7 @@ function collisionY() {
       } else {
         dx = 8;
       }
+      playSound(beepPaddle);
       return true;
     }
   }
@@ -178,6 +199,7 @@ function gameOver() {
   cancelAnimationFrame(timer);
   score.innerHTML += "     Game Over!";
   score.style.backgroundColor = "rgb(128, 0, 0)";
+  playSound(beepGameOver);
 }
 
 function mouseDown(e) {
@@ -238,4 +260,62 @@ function newGame() {
   dx = 2;
   setDifficulty(difficultySelect.selectedIndex);
   score.style.backgroundColor = "rgb(32, 128, 64)";
+}
+
+function initAudio() {
+  // load audio files
+  beepX = new Audio("./sounds/beepX.mp3");
+  beepY = new Audio("./sounds/beepY.mp3");
+  beepPaddle = new Audio("./sounds/beepPaddle.mp3");
+  beepGameOver = new Audio("./sounds/beepGameOver.mp3");
+  bgMusic = new Audio("./sounds/music.mp3");
+  // turn off volume
+  beepX.volume = 0;
+  beepY.volume = 0;
+  beepPaddle.volume = 0;
+  beepGameOver.volume = 0;
+  bgMusic.volume = 0;
+  // grant permission
+  beepX.play();
+  beepY.play();
+  beepPaddle.play();
+  beepGameOver.play();
+  bgMusic.play();
+  // this stores in memory for later
+  beepX.pause();
+  beepY.pause();
+  beepPaddle.pause();
+  beepGameOver.pause();
+  bgMusic.pause();
+  // this sets volume for next time
+  beepX.volume = 1;
+  beepY.volume = 1;
+  beepPaddle.volume = 1;
+  beepGameOver.volume = 1;
+  bgMusic.volume = 1;
+}
+
+function toggleSound() {
+    if(beepX === null) {
+        initAudio();
+    }
+    sndEnabled = !sndEnabled;
+}
+
+function playSound(objSound) {
+    if(sndEnabled)
+    objSound.play();
+}
+
+function toggleMusic() {
+    if(bgMusic == null) {
+        initAudio();
+    }
+    if (musicEnabled) {
+        bgMusic.pause();
+    } else {
+        bgMusic.loop = true;
+        bgMusic.play();
+    }
+    musicEnabled = !musicEnabled;
 }
